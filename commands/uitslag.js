c7392ce2667bf2880c72e1228c5d1bedcd0c9bad
TimRaben <87567520+TimRaben@users.noreply.github.com>
2022-07-13 12:37:47 +0200

    i
    i

--- commands/uitslag.js
+++ commands/uitslag.js
@@ -82,6 +82,19 @@ module.exports.run = async (client, message, args) => {
 
 } 
 
+async function promptMessage(message, author, time, reactions) {
+    time *=1000;
+
+    for (const reaction of reactions) {
+        await message.react(reaction);
+    }
+
+    const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+    return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
+
+}
+
 
 
 module.exports.help = {
